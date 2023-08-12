66e5850f7e8de2623342948f6829226fc8d8e878
I A <ilie.adrian1986@gmail.com>
2023-08-12 22:34:02 +0300

    basic version of increasing opacity functionality

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ let createBtn = document.getElementById("create");
 let resetBtn = document.getElementById("reset");
 let colorPicker = document.getElementById("picker");
 let rainbowModeBtn = document.getElementById("rainbow-mode");
+let opacityBtn = document.getElementById("opacity");
 let currentColour;
 let baseColour = "#3e4c75";
+let opacityValue = 0.1;
 let userPickedColour;
 let rainbowModeOn = false;
 let userInput = 16;
 let cells;
+
 
 function drawCanvas(cells) {
     cells = cells || userInput;
@@ -45,11 +48,33 @@ function updateWithUserInput(userInput){
         drawCanvas(userInput); // Update the canvas with the new user input value
 }
 
-function setColour(event) {
-    const cell = event.target;
+function increasOpacitymode(event) {
+    canvas.addEventListener("mouseover", function(event) {
+        const cell = event.target;
+        if (cell.classList.contains("cell")) {
+            cell.style.backgroundColor = currentColour; // Set the current color
+            
+            // Increase the opacity
+            cell.style.opacity = opacityValue.toString();
+            
+            // Increment the opacity value for the next hover
+            opacityValue += 0.1;
+            
+            // Reset the opacity value when it goes beyond 1
+            if (opacityValue > 1) {
+                opacityValue = 0.1;
+            }
+        }
+    });
+}
 
+function setColour(event) {
+    // Reset the opacity value when a new cell is hovered
+    opacityValue = 0.1;
+    
+    const cell = event.target;
     if (rainbowModeOn) {
-        const randomColor = getRandomRRGGBBAA();
+        const randomColor = getRandomRGBA();
         cell.style.background = randomColor;
     } else if (userPickedColour) {
         cell.style.background = userPickedColour;
@@ -58,13 +83,25 @@ function setColour(event) {
     }
 }
 
-function getRandomRRGGBBAA() {
+// function setColour(event) {
+//     const cell = event.target;
+
+//     if (rainbowModeOn) {
+//         const randomColor = getRandomRGBA();
+//         cell.style.background = randomColor;
+//     } else if (userPickedColour) {
+//         cell.style.background = userPickedColour;
+//     } else {
+//         cell.style.background = baseColour;
+//     }
+// }
+
+function getRandomRGBA() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
 
-    const colorHex = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}FF`;
-    return colorHex;
+    return `rgba(${r},${g},${b}, 1)`;    
 }
 
 function watchColorPicker() {
@@ -81,6 +118,7 @@ function resetCanvas() {
 }
 
 createBtn.addEventListener("click", takeUserInput);
+opacityBtn.addEventListener("click", increasOpacitymode)
 colorPicker.addEventListener("change", watchColorPicker);
 rainbowModeBtn.addEventListener("click", () => rainbowModeOn = true);
 resetBtn.addEventListener("click", resetCanvas);
